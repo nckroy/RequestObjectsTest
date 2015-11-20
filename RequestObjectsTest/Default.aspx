d233211b82ae82c13e20262a91894723105e7a0a
Nick Roy <nroy@i2-denv-103.internet2.edu>
2015-11-20 16:16:49 -0700

    First commit

---
+++ RequestObjectsTest/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RequestObjectsTest._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+    <style type="text/css">
+        .style1 {
+            font-size: large;
+            font-weight: bold;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .style2
+        {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ObjectDataSource ID="RequestObjectsDS" runat="server" 
+            SelectMethod="GetServerVars" TypeName="RequestObjectsTest._Default">
+        </asp:ObjectDataSource>
+        <asp:Image ID="RObjLogo" runat="server" AlternateText="RequestObjectsTest" 
+            ImageUrl="~/Resources/logo.png" />
+        <br />
+        <br />
+        <span class="style1">Server Variables</span><asp:GridView ID="RequestObjectsGV" 
+            runat="server" AutoGenerateColumns="False" DataSourceID="RequestObjectsDS" 
+            CssClass="style2">
+            <RowStyle BackColor="#FFF4EA" Font-Names="Arial,Helvetica,Sans-Serif" />
+            <Columns>
+                <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                <asp:BoundField DataField="Value" HeaderText="Value" SortExpression="Value" />
+            </Columns>
+            <HeaderStyle Font-Names="Arial,Helvetica,Sans-Serif" />
+            <AlternatingRowStyle BackColor="#FFCC99" />
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ RequestObjectsTest/Default.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace RequestObjectsTest
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        public List<NameValuePair> GetServerVars()
+        {
+			NameValueCollection qs = Request.QueryString;
+			string returnFilter = MQueryString.GetReturnFilter(qs);
+            List<NameValuePair> serverVars = new List<NameValuePair>();
+			if (!string.IsNullOrEmpty (returnFilter)) 
+			{
+				foreach (string s in Context.Items.Keys.OfType<string>().Where(c => c.Contains(returnFilter)))
+				{
+					serverVars.Add(new NameValuePair(s, Context.Items[s].ToString()));
+				}
+				serverVars.Sort();
+			} 
+			else 
+			{
+				foreach (string s in Context.Items.Keys.OfType<string>())
+				{
+					serverVars.Add(new NameValuePair(s, Context.Items[s].ToString()));
+				}
+				serverVars.Sort();
+			}
+            return serverVars;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            foreach (string s in Request.ServerVariables.AllKeys)
+            {
+                Context.Items[s] = Request.ServerVariables[s];
+            }
+            RequestObjectsGV.DataBind();
+        }
+    }
+
+	public static class MQueryString
+	{
+		public static string GetReturnFilter(NameValueCollection qs)
+		{
+			string returnFilter = null;
+			if(!string.IsNullOrEmpty(qs["filter"]))
+			{
+				returnFilter = qs["filter"];
+			}
+			return returnFilter;
+		}
+	}
+
+    public class NameValuePair : IComparable
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public NameValuePair(string Name, string Value)
+        {
+            this.Name = Name;
+            this.Value = Value;
+        }
+
+        #region IComparable Members
+
+        public int CompareTo(object obj)
+        {
+            NameValuePair nvp = (NameValuePair)obj;
+            return this.Name.CompareTo(nvp.Name);
+        }
+
+        #endregion
+    }
+}
